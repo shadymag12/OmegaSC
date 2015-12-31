1f1069cd9b015c6d1f9d0ea083bad4ea7854a945
Vaibhav <vaibhav.bharat5@gmail.com>
2015-12-31 16:48:23 +0530

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+
+  devise_for :admins
+  devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        :omniauth_callbacks => "users/omniauth_callbacks" 
+      }
   root to: "home#index", as: :user
   get 'autocomplete_product_name', to: 'home#autocomplete_product_name', as: :autocomplete_product_name
   get 'maths',to: 'home#maths', as: :maths
   get 'physics',to: 'home#physics', as: :physics
   get 'chemistry',to: 'home#chemistry', as: :chemistry
+  post 'setlocat', to: 'home#setlocat', as: :setlocat
+ 
 end
